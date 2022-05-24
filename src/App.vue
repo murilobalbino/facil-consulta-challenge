9bf0082046735c732678d043199fb83749af323f
Murilo Balbino <murilobalcontato@gmail.com>
2022-05-24 16:57:38 -0300

    creating project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
